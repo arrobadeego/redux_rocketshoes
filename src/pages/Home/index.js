3096682c97a3089ea77007709ca3847c6ecf2f23
Diego Moreira <diegomptcc@hotmail.com>
2019-09-06 10:16:43 -0300

    include ADD_TO_CART  dispatch on Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import { formatPrice } from '../../util/format';
@@ -7,7 +9,7 @@ import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
     // eslint-disable-next-line react/state-in-constructor
     state = {
         products: [],
@@ -24,6 +26,15 @@ export default class Home extends Component {
         this.setState({ products: data });
     }
 
+    handleAddProduct = product => {
+        const { dispatch } = this.props;
+
+        dispatch({
+            type: 'ADD_TO_CART',
+            product,
+        });
+    };
+
     render() {
         const { products } = this.state;
 
@@ -35,7 +46,10 @@ export default class Home extends Component {
                         <strong>{product.title}</strong>
                         <span>{product.priceFormatted}</span>
 
-                        <button type="button">
+                        <button
+                            type="button"
+                            onClick={() => this.handleAddProduct(product)}
+                        >
                             <div>
                                 <MdAddShoppingCart size={16} color="#FFF" /> 3
                             </div>
@@ -48,3 +62,5 @@ export default class Home extends Component {
         );
     }
 }
+
+export default connect()(Home);
