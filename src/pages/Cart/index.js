684288365f9c64da184488d21132ef8be5fd2520
Diego Moreira <diegomptcc@hotmail.com>
2019-09-06 07:55:57 -0300

    style Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -10,47 +10,56 @@ import { Container, ProductTable, Total } from './styles';
 export default function Cart() {
     return (
         <Container>
-            <ProductTable />
-            <thead>
-                <tr>
-                    <th />
-                    <th>PRODUTO</th>
-                    <th>QTD</th>
-                    <th>SUBTOTAL</th>
-                    <th />
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <img
-                            src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
-                            alt="Tênis"
-                        />
-                    </td>
-                    <td>
-                        <strong>Tênis roxeda</strong>
-                        <span>R$129,90</span>
-                    </td>
-                    <td>
-                        <button type="button">
-                            <MdRemoveCircleOutline size={20} color="#7159c1" />
-                        </button>
-                        <input type="number" readOnly value={1} />
-                        <button type="button">
-                            <MdAddCircleOutline size={20} color="#7159c1" />
-                        </button>
-                    </td>
-                    <td>
-                        <strong>R$258,80</strong>
-                    </td>
-                    <td>
-                        <button type="button">
-                            <MdDelete size={20} color="#7159c1" />
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBTOTAL</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img
+                                src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
+                                alt="Tênis"
+                            />
+                        </td>
+                        <td>
+                            <strong>Tênis roxeda</strong>
+                            <span>R$129,90</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                                <input type="number" readOnly value={1} />
+                                <button type="button">
+                                    <MdAddCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$258,80</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
             <footer>
                 <button type="button">Finalizar pedido</button>
                 <Total>
