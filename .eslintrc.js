4e305ffb57478719ed6251fddcf78ff6363f7414
Diego Moreira <diegomptcc@hotmail.com>
2019-09-09 17:42:47 -0300

    include new hooks rules on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,18 +17,21 @@ module.exports = {
       ecmaFeatures: {
         jsx: true,
       },
-      ecmaVersion: 2018,
-      sourceType: 'module',
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
     plugins: [
-      'react',
-      'prettier'
+        'react',
+        'prettier',
+        'react-hooks'
     ],
     rules: {
         'prettier/prettier': 'error',
         'react/jsx-filename-extension': [ 'warn', { extensions: ['.jsx', '.js']}],
-      'import/prefer-default-export': 'off',
-      'no-param-reassign': 'off',
-      'no-console': [ 'error', { allow: ["tron"]}]
+        'import/prefer-default-export': 'off',
+        'no-param-reassign': 'off',
+        'no-console': [ 'error', { allow: ["tron"]}],
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn'
     },
-  };
+};
